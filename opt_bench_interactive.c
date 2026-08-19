ad6f439d3bce08673949e0e80c7fc53a7497b852
Antti Rytinki <antti.rytinki@outlook.com>
2026-08-19 16:25:31 +0200

    Benchmark update 2026-08-19_16-25-31

--- opt_bench_interactive.c
+++ opt_bench_interactive.c
@@ -1,6 +1,6 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
 
 #include "optimizers.h"
@@ -8,102 +8,125 @@
 
 #define RUNS 50
 
-static double Distance(const double *a, const double *b, int dim)
+static const int Checkpoints[] = {
+    100,
+    300,
+    1000,
+    3000,
+    10000,
+    30000,
+    100000
+};
+
+#define CHECKPOINT_COUNT \
+    ((int)(sizeof(Checkpoints) / sizeof(Checkpoints[0])))
+
+static void BenchmarkConvergence(
+    InteractiveOptimizer* optimizer,
+    const TestProblem* problem,
+    double* averages)
 {
-    double sum = 0.0;
+    double totals[CHECKPOINT_COUNT] = { 0.0 };
 
-    for (int i = 0; i < dim; i++)
-    {
-        double d = a[i] - b[i];
-        sum += d * d;
-    }
-
-    return sqrt(sum);
-}
-
-static void BenchmarkInteractive(
-    InteractiveOptimizer *optimizer,
-    const TestProblem *problem,
-    int maxEvaluations)
-{
-    double totalValue = 0.0;
-    double totalDistance = 0.0;
-    double bestValue = INFINITY;
-    double worstValue = -INFINITY;
-    double totalTime = 0.0;
-
-    for (int run = 0; run < RUNS; run++)
-    {
+    for (int run = 0; run < RUNS; run++) {
         double bestX;
         double bestY;
         double value;
         int evaluations;
 
-        clock_t start = clock();
-
         optimizer->Init(problem);
 
-        do
-        {
+        optimizer->GetBest(
+            &bestX,
+            &bestY,
+            &value,
+            &evaluations);
+
+        int checkpointIndex = 0;
+
+        while (checkpointIndex < CHECKPOINT_COUNT) {
             optimizer->Step();
-            optimizer->GetBest(&bestX, &bestY, &value, &evaluations);
+
+            optimizer->GetBest(
+                &bestX,
+                &bestY,
+                &value,
+                &evaluations);
+
+            while (
+                checkpointIndex < CHECKPOINT_COUNT
+                && evaluations >= Checkpoints[checkpointIndex]) {
+                totals[checkpointIndex] += value;
+                checkpointIndex++;
+            }
         }
-        while (evaluations < maxEvaluations);
-
-        clock_t end = clock();
-
-        double found[MAX_DIM];
-        found[0] = bestX;
-        found[1] = bestY;
-
-        double distance = Distance(found, problem->optimum, problem->dim);
-        double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-
-        totalValue += value;
-        totalDistance += distance;
-        totalTime += seconds;
-
-        if (value < bestValue)
-            bestValue = value;
-
-        if (value > worstValue)
-            worstValue = value;
     }
 
-    printf(
-        "%-16s %-22s avg value: %12.6f  best: %12.6f  worst: %12.6f  avg distance: %10.6f  avg time: %.6f s\n",
-        problem->name,
-        optimizer->name,
-        totalValue / RUNS,
-        bestValue,
-        worstValue,
-        totalDistance / RUNS,
-        totalTime / RUNS);
+    for (int i = 0; i < CHECKPOINT_COUNT; i++)
+        averages[i] = totals[i] / RUNS;
+}
+
+static void PrintHeader(void)
+{
+    printf("%-24s", "Algorithm");
+
+    for (int i = 0; i < CHECKPOINT_COUNT; i++)
+        printf(" %12d", Checkpoints[i]);
+
+    printf("\n");
+
+    printf("%-24s", "------------------------");
+
+    for (int i = 0; i < CHECKPOINT_COUNT; i++)
+        printf(" %12s", "------------");
+
+    printf("\n");
+}
+
+static void PrintResult(
+    const char* name,
+    const double* averages)
+{
+    printf("%-24s", name);
+
+    for (int i = 0; i < CHECKPOINT_COUNT; i++)
+        printf(" %12.6f", averages[i]);
+
+    printf("\n");
 }
 
 int main(void)
 {
     srand((unsigned int)time(NULL));
 
-    TestProblem *problems[] =
-    {
+    TestProblem* problems[] = {
         &SphereProblem,
         &RastriginProblem,
         &RosenbrockProblem
     };
 
-    int problemCount =
-        sizeof(problems) / sizeof(problems[0]);
-
-    int maxEvaluations = 100000;
+    int problemCount = sizeof(problems) / sizeof(problems[0]);
 
     printf("Runs per algorithm: %d\n", RUNS);
-    printf("Max evaluations per run: %d\n\n", maxEvaluations);
+    printf("Metric: average best value reached\n\n");
 
-    for (int p = 0; p < problemCount; p++)
-    {
-        for (int i = 0; i < OptimizerCount; i++)
-            BenchmarkInteractive(Optimizers[i], problems[p], maxEvaluations);
+    for (int p = 0; p < problemCount; p++) {
+        printf("%s\n\n", problems[p]->name);
+
+        PrintHeader();
+
+        for (int i = 0; i < OptimizerCount; i++) {
+            double averages[CHECKPOINT_COUNT];
+
+            BenchmarkConvergence(
+                Optimizers[i],
+                problems[p],
+                averages);
+
+            PrintResult(
+                Optimizers[i]->name,
+                averages);
+        }
 
         printf("\n");
     }
