1cabd7a75874c487516b8c521ef129e87376f38c
Antti Rytinki <antti.rytinki@outlook.com>
2026-08-19 16:19:43 +0200

    Benchmark update 2026-08-19_16-19-43

--- build.sh
+++ build.sh
@@ -2,7 +2,17 @@
 
 printf '\e[8;48;160t'
 
-gcc -Wall -Wextra -O2 *.c -lm -o opt_bench || exit 1
+gcc -Wall -Wextra -O2 \
+    opt_bench_interactive.c \
+    optimizers.c \
+    problems.c \
+    ExploreExploitInteractive.c \
+    RandomSearchInteractive.c \
+    HillClimbInteractive.c \
+    DifferentialEvolutionInteractive.c \
+    DirectionalTreeInteractive.c \
+    -lm \
+    -o opt_bench || exit 1
 
 ./opt_bench
 
@@ -12,3 +22,11 @@ DESTINATION="History/Optim-$TIMESTAMP"
 mkdir -p "$DESTINATION"
 
 find . -maxdepth 1 -type f -exec cp {} "$DESTINATION" \;
+
+git add .
+
+if ! git diff --cached --quiet
+then
+    git commit -m "Benchmark update $TIMESTAMP"
+    git push
+fi
